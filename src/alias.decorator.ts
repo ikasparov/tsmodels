c6c59ebc8ef99cfb16a13e975aab9b780389c4f2
ikasparov <ikasparov.dev@gmail.com>
2017-08-18 12:02:41 +0300

    Add nested model convertation

--- src/alias.decorator.ts
+++ src/alias.decorator.ts
@@ -1,6 +1,13 @@
-export function Alias(alias: string) {
+import { ApplicationModel as AppModel } from './application-model';
+import { Type } from './type';
+
+export function Alias<T extends AppModel>(alias?: string, type?: Type<T>) {
   return function (target: Object, propertyKey: string | symbol) {
     target.constructor['_alias'] = target['constructor']['_alias'] || [];
-    target.constructor['_alias'].push({ key: propertyKey, value: alias });
+    target.constructor['_alias'].push({
+      key: propertyKey,
+      value: alias || propertyKey,
+      type: type
+    });
   };
 }
